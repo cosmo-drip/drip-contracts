781a703f789ae9a765abf7254819a4434e32d9e3
Raumo0 <dharapko@gmail.com>
2025-08-09 06:19:12 +0200

    feat(disburser): add robust initialization parameters to InstantiateMsg
    Refs: [DRIP-96]

--- contracts/disburser/src/msg.rs
+++ contracts/disburser/src/msg.rs
@@ -1,7 +1,24 @@
 use cosmwasm_schema::{cw_serde, QueryResponses};
+use cosmwasm_std::Coin;
+use cw_utils::Expiration;
 
 #[cw_serde]
-pub struct InstantiateMsg {}
+pub struct WithdrawalTtl {
+    pub default_sec: u64,
+    pub max_sec: u64,
+}
+
+#[cw_serde]
+pub struct InstantiateMsg {
+    pub settlement_asset_limit: Coin,
+    pub quote_asset_limit: Coin,
+    pub admin: Option<String>,
+    pub recipient_addr: String,
+    pub price_feeder_addr: String,
+    pub payment_initiator_addrs: Vec<String>,
+    pub funding_expiration: Expiration,
+    pub withdrawal_ttl: WithdrawalTtl,
+}
 
 #[cw_serde]
 pub enum ExecuteMsg {}
