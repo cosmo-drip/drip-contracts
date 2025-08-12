8e5862cdd5f202eb06f4db4df57372bf8dc8cd42
Raumo0 <dharapko@gmail.com>
2025-08-13 01:57:13 +0200

    feat(drip-price-oracle): add new price oracle contract scaffold Refs: [DRIP-124]

---
+++ contracts/drip-price-oracle/src/contract.rs
@@ -0,0 +1,42 @@
+#[cfg(not(feature = "library"))]
+use cosmwasm_std::{entry_point, Binary, Deps, DepsMut, Env, MessageInfo, Response, StdResult};
+use cw2::set_contract_version;
+use crate::ContractError;
+use crate::msg::{ExecuteMsg, InstantiateMsg, QueryMsg};
+
+// version info for migration info
+const CONTRACT_NAME: &str = "crates.io:drip-price-oracle";
+const CONTRACT_VERSION: &str = env!("CARGO_PKG_VERSION");
+
+#[cfg_attr(not(feature = "library"), entry_point)]
+pub fn instantiate(
+    deps: DepsMut,
+    _env: Env,
+    _info: MessageInfo,
+    _msg: InstantiateMsg,
+) -> Result<Response, ContractError> {
+    // todo:
+    // version for migrations
+    set_contract_version(deps.storage, CONTRACT_NAME, CONTRACT_VERSION)?;
+    Ok(Response::new().add_attribute("action", "instantiate"))
+}
+
+#[cfg_attr(not(feature = "library"), entry_point)]
+pub fn execute(
+    _deps: DepsMut,
+    _env: Env,
+    _info: MessageInfo,
+    msg: ExecuteMsg,
+) -> Result<Response, ContractError> {
+    match msg {
+        ExecuteMsg::UpdateAdmin { admin } => unimplemented!(),
+    }
+}
+
+#[cfg_attr(not(feature = "library"), entry_point)]
+pub fn query(_deps: Deps, _env: Env, _msg: QueryMsg) -> StdResult<Binary> {
+    unimplemented!()
+}
+
+#[cfg(test)]
+mod tests {}
---
+++ contracts/drip-price-oracle/src/lib.rs
@@ -0,0 +1,7 @@
+pub mod contract;
+mod error;
+pub mod helpers;
+pub mod state;
+pub mod msg;
+
+pub use crate::error::ContractError;
